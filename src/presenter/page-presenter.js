fdb9c15f2ec6a511e7fa97f59931933525182630
Kate <stolyarova.katy@ya.ru>
2022-08-14 17:01:42 +0500

    Настройка окружения

---
+++ src/presenter/page-presenter.js
@@ -0,0 +1,24 @@
+import FiltersView from "../view/filters-view.js";
+import SortingView from "../view/sorting-view.js";
+import ListView from "../view/list-view.js";
+import PointView from "../view/point-view.js";
+import editFormView from "../view/edit-form-view";
+import { render } from "../render.js";
+
+export default class PagePresenter {
+  listComponent = new ListView();
+
+  init = (filtersContainer, eventsContainer) => {
+    this.filtersContainer = filtersContainer;
+    this.eventsContainer = eventsContainer;
+
+    render(new FiltersView(), filtersContainer);
+    render(new SortingView(), eventsContainer);
+    render(this.listComponent, eventsContainer);
+    render(new editFormView(), this.listComponent.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      render(new PointView(), this.listComponent.getElement());
+    }
+  };
+}
