65aa5c99dc15de64f2c314bcd7cc6d647c0aa7bd
Kate <stolyarova.katy@ya.ru>
2022-08-14 17:33:06 +0500

    Время разбивать камни

--- src/view/filters-view.js
+++ src/view/filters-view.js
@@ -1,4 +1,4 @@
-import { createElement } from "../render.js";
+import { createElement } from '../render.js';
 
 const createFiltersTemplate = () =>
   `<form class="trip-filters" action="#" method="get">
