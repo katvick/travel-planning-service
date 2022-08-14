27a00a439d2346e7b6dbdc091aaaf3381a972a51
Kate <stolyarova.katy@ya.ru>
2022-08-14 17:31:10 +0500

    Время разбивать камни

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import PagePresenter from './presenter/page-presenter.js';
 
 const filtersElement = document.querySelector('.trip-controls__filters'); //
 const EventsElement = document.querySelector('.trip-events');
-// const pointContainer = tripEventsContainer.querySelector('.trip-events__list');
-// const editContainer = pointContainer.querySelector('.trip-events__item');
 const pagePresenter = new PagePresenter();
 
 pagePresenter.init(filtersElement, EventsElement);
