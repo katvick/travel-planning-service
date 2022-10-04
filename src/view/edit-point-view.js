6cfd4ed590a1f369453625b53d14207635770adf
Kate <stolyarova.katy@ya.ru>
2022-10-04 22:11:56 +0500

    отладка появления офферов

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -134,7 +134,7 @@ const createEditPointTemplate = (point, listOffers, listDestinations) => {
       
       </header>
       <section class="event__details">
-        ${offersByType.length > 0 ? `
+        ${offersByType ? `
           <section class="event__section  event__section--offers">
             <h3 class="event__section-title  event__section-title--offers">Offers</h3>
             <div class="event__available-offers">
